84ff5d48c12df2672ce84a5376eba4c0e328ce74
vtrtbbs <zhang.z.y@163.com>
2018-09-19 00:06:37 +0800

    uuuu

--- src/test/java/hello/SimpleControllerTests.java
+++ src/test/java/hello/SimpleControllerTests.java
@@ -1,10 +1,18 @@
 package hello;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import java.net.HttpCookie;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,10 +25,14 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.springstudy.domain.Country;
 import com.springstudy.service.DemoService;
+import com.springstudy.service.TestService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -33,7 +45,8 @@ public class SimpleControllerTests {
 	private WebApplicationContext wac;
 	@Autowired
 	DemoService demoService;
-	
+	@Autowired
+	TestService testService;
 
 	private MockMvc mockMvc;
 	private MockHttpSession session;
@@ -46,15 +59,23 @@ public class SimpleControllerTests {
 
 	@Test
 	public void getTestMsg() throws Exception {
-		this.mockMvc.perform((get("/hello")).accept(MediaType.ALL)).andExpect(status().isOk()).andDo(print()); // print
+		this.mockMvc.perform((get("/hello")).accept(MediaType.ALL)).andExpect(status().isOk()).andDo(print());
 	}
 	
 	@Test
 	public void insert() {
-		Country country = new Country();
+		List<Country> list = testService.list();		
+		System.out.println("------list----->"+list);
+		/*Country country = new Country();
 		country.setAbbr("2222");
 		country.setName("adfdfd");
-		demoService.save(country);
+		demoService.save(country);*/
+		HttpSession session=null;  
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		HttpSession contextSess = attr == null ? session : attr.getRequest().getSession(true);
+		
+		Cookie[] cookie = null;
+		Cookie[] contextCookie = attr == null ? cookie : attr.getRequest().getCookies();
+		Assert.notNull(list, "cookie[] is null");		 
 	}
-
 }
